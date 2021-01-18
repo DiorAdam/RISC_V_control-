11024470615068b87e6ce47742d851867bc4bba9
fadel <fadelsougou@gmail.com>
2021-01-18 17:04:15 +0100

    fix ori.s

--- implem/program/autotest/ori.s
+++ implem/program/autotest/ori.s
@@ -1,14 +1,15 @@
 # TAG = ori
 	.text
 
-	ori x31, x0, 0xfff        # Test yolo
-    ori x31, x0, 0
-    ori x31, x0, 0x145
+	ori x31, x0, 0xff         # Test yolo
+    ori x31, x0, 0            # Test yolo
+    ori x31, x0, 0x145        # Test yolo
 	
 
 	# max_cycle 50
 	# pout_start
-	# 00000fff
+	# 000000ff
     # 00000000
     # 00000145
 	# pout_end
+    
