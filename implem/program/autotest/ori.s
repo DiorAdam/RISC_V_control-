b67e33c77dd4261d157078da6b1f046265cd5ca4
Sougou Fadel <mouhamed.sougou@student-cs.fr>
2021-01-18 17:06:57 +0100

    fix ori.s

--- implem/program/autotest/ori.s
+++ implem/program/autotest/ori.s
@@ -2,14 +2,14 @@
 	.text
 
 	ori x31, x0, 0xff         # Test yolo
-    ori x31, x0, 0            # Test yolo
-    ori x31, x0, 0x145        # Test yolo
+	ori x31, x0, 0            # Test yolo
+	ori x31, x0, 0x145        # Test yolo
 	
 
 	# max_cycle 50
 	# pout_start
 	# 000000ff
-    # 00000000
-    # 00000145
+	# 00000000
+	# 00000145
 	# pout_end
     
