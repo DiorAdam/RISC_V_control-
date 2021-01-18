c4aa816d2b8ad0d3e9283d9a421fd75ca7207f50
fadel <fadelsougou@gmail.com>
2021-01-18 15:19:12 +0100

    fix addi.s : test verif

--- implem/program/autotest/addi.s
+++ implem/program/autotest/addi.s
@@ -1,16 +1,15 @@
 # TAG = addi
+    .text
 
-.text
+    addi x31, x0, 0;
+    addi x31, x31, 0x1;
+    addi x31, x31, 0x2;
+    addi x31, x31, 0x41;
 
-addi x31, x0, 0;
-addi x31, x31, 0x1;
-addi x31, x31, 0x2;
-addi x31, x31, 0x41;
-
-# max_cycle 50
-	# pout_start
-	# 00000000
-	# 00000001
-	# 00000003
+    # max_cycle 50
+    # pout_start
+    # 00000000
+    # 00000001
+    # 00000003
     # 00000044
-	# pout_end
+    # pout_end
