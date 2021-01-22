d6da2765a7c80c8a34c4faf497f850a1df264e43
fadel <fadelsougou@gmail.com>
2021-01-22 13:41:27 +0100

    adding tests for ori

--- implem/program/autotest/ori.s
+++ implem/program/autotest/ori.s
@@ -4,12 +4,13 @@
 	ori x31, x0, 0xff         # Test yolo
 	ori x31, x0, 0            # Test yolo
 	ori x31, x0, 0x145        # Test yolo
-	
+	ori x31, x31, 0x27
 
 	# max_cycle 50
 	# pout_start
 	# 000000ff
 	# 00000000
 	# 00000145
+	# 0000015f
 	# pout_end
     
