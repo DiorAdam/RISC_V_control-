76524be028069351808d13d09330dc7262398ad8
Sougou Fadel <mouhamed.sougou@student-cs.fr>
2021-01-24 23:05:18 +0100

    Update lw.s

--- implem/program/autotest/lw.s
+++ implem/program/autotest/lw.s
@@ -2,21 +2,25 @@
     .text
 
     auipc x29, 0
-    lw x31, 0x14(x29)
-    lw x31, 0x24(x29)
+    add x30, x1, x2
+    lw x31, 0(x29)
 
-    add x30, x1, x2
-    add x30, x1, x2
-    add x30, x1, x2
+    auipc x29, 0
+    add x30, x3, x4
+    lw x31, 0(x29)
 
-    add x30, x3, x4
-    add x30, x3, x4
-    add x30, x3, x4
-    add x30, x3, x4
-    add x30, x3, x4
+    auipc x29, 0
+    lw x31, 0xc(x29)
+
+    add x30, x5, x6
+    add x30, x5, x6
+    add x30, x5, x6
+    add x30, x5, x6
+    
 
     # max_cycle 50
     # pout_start
     # 00208f33
     # 00418F33
+    # 00628f33
     # pout_end
