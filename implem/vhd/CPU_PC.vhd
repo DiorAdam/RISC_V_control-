0a3bd0060068d2f907d2a6b68bae207aeed81823
fadel <fadelsougou@gmail.com>
2021-01-25 11:02:41 +0100

    fix load word

--- implem/vhd/CPU_PC.vhd
+++ implem/vhd/CPU_PC.vhd
@@ -347,20 +347,22 @@ begin
                 --calculating memory address
                 cmd.AD_Y_sel <= AD_Y_immI;
                 cmd.AD_we <= '1';
-                --writing data from memory on destination register
-                cmd.ADDR_sel <= ADDR_from_ad;
-                cmd.mem_ce <= '1';
-                cmd.mem_we <= '0';
+                
                 --next state
                 state_d <= S_LW_exit;
             
             when S_LW_exit =>
-                cmd.RF_SIZE_sel <= RF_SIZE_word;
-                cmd.RF_we <= '1';
-                cmd.DATA_sel <= DATA_from_mem;
+                --writing data from memory on destination register
+                cmd.ADDR_sel <= ADDR_from_ad;
+                cmd.mem_ce <= '1';
+                cmd.mem_we <= '0';
+                
                 state_d <= S_LW3;
 
             when S_LW3 =>
+                cmd.RF_SIZE_sel <= RF_SIZE_word;
+                cmd.RF_we <= '1';
+                cmd.DATA_sel <= DATA_from_mem;
                 --lecture mem[PC]
                 cmd.ADDR_sel <= ADDR_from_pc;
                 cmd.mem_ce <= '1';
