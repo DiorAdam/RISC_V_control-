6e7e307c59bce8ef9a1434b8eb710547244341a6
augustin <augustin.basse@student-cs.fr>
2021-01-25 15:04:09 +0100

    ajout test srai

---
+++ implem/program/autotest/srai.s
@@ -0,0 +1,14 @@
+# TAG = srai
+    .text
+    addi x1, x0, 0x290
+    srai x31, x1, 0x4
+
+    addi x1, x0, 0xf
+    srai x31, x1, 0x2
+
+
+    # max_cycle 50
+    # pout_start
+    # 00000029
+    # 00000003
+    # pout_end
