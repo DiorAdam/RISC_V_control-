97c5d820ea2c154af2b9b55f0e0b886661c09642
augustin <augustin.basse@student-cs.fr>
2021-01-25 11:19:03 +0100

    ajout test sra

--- implem/vhd/CPU_PC.vhd
+++ implem/vhd/CPU_PC.vhd
@@ -33,7 +33,8 @@ architecture RTL of CPU_PC is
         S_ADD,
         S_ORI,
         S_SRL,
-	S_SLL
+	S_SLL,
+	S_SRA
     );
 
     signal state_d, state_q : State_type;
@@ -213,6 +214,12 @@ begin
 		    cmd.PC_we <= '1';
 		    state_d <= S_SLL;
 
+		elsif status.IR(6 downto 0) = "0110011" and status.IR(14 downto 12) = "101" and status.IR(31 downto 25) = "0100000" then
+		    cmd.TO_PC_Y_sel <= TO_PC_Y_cst_x04;
+		    cmd.PC_sel <= PC_from_pc;
+		    cmd.PC_we <= '1';
+		    state_d <= S_SRA;
+
                 else
                     state_d <= S_Error; -- Pour d´etecter les rat´es du d´ecodage
                 end if;
@@ -297,6 +304,19 @@ begin
 		-- rd <- décalage a gauche rs1 par rs2
 		cmd.SHIFTER_Y_SEL <= SHIFTER_Y_rs2;
 		cmd.SHIFTER_op <=SHIFT_ll;
+		cmd.RF_we <= '1';
+		cmd.DATA_sel <= DATA_from_shifter;
+		--lecture mem[PC]
+		cmd.ADDR_sel <= ADDR_from_pc;
+		cmd.mem_ce <= '1';
+                cmd.mem_we <= '0';
+		--next state
+		state_d <= S_Fetch;
+
+	   when S_SRA =>
+		-- rd <- décalage a droite rs1 par rs2 avec signe
+		cmd.SHIFTER_Y_SEL <= SHIFTER_Y_rs2;
+		cmd.SHIFTER_op <=SHIFT_ra;
 		cmd.RF_we <= '1';
 		cmd.DATA_sel <= DATA_from_shifter;
 		--lecture mem[PC]
